ce7ac4ac84f7aafd3d81ebf8f10c128135e305c7
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-23 03:57:31 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,15 +6,14 @@
 # suite_e: "Crumpet the Elf"
 # }
 
-def select_winner (passengers)
+def select_winner(passengers)
   winner = ""
 passengers.each do |suite, name|
-   if suite == :suite_a && name.start_with? 
-  ("A")
+   if suite == :suite_a && name.start_with?("A")
      winner = name 
   end 
 end 
-winners
+winner
 end 
 
 # add the code snippet here!
